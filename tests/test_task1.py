94d135988a88fbe8df5f063dc9e9d3a5d609a3f1
4isto <4istopopikoly@vk.com>
2025-11-23 23:33:53 +0300

    add tests

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import pytest
 from printer_function import printer
 
@@ -13,7 +14,7 @@ def test_printer_return_type():
 def test_printer_annotations():
     annotations = printer.__annotations__
     assert annotations["value"] == str
-    assert annotations["return"] is None  # Исправлено
+    assert annotations["return"] is None
 
 
 def test_printer_output(capsys: pytest.CaptureFixture[str]):
--- tests/test_task2.py
+++ tests/test_task2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 from universal_pair import UniversalPair
 
 
