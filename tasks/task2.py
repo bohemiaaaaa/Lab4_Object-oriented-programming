f012a110f0b0187dbb85f9578076bf1d47ea070f
4isto <4istopopikoly@vk.com>
2025-11-30 22:22:59 +0300

    update task2

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from universal_pair_package.universal_pair import UniversalPair
 
 
 def main():
-    # С целыми числами
-    int_pair = UniversalPair[int](10, 20)
+    # С целыми числами - тип автоматически определяется как int
+    int_pair = UniversalPair(10, 3)
     print(f"Пара целых чисел: {int_pair.get_pair()}")
 
-    # Со строками
-    str_pair = UniversalPair[str]("hello", "world")
+    # Со строками - тип автоматически определяется как str
+    str_pair = UniversalPair("hello", "world")
     print(f"Пара строк: {str_pair.get_pair()}")
 
-    # С числами с плавающей точкой
-    float_pair = UniversalPair[float](3.14, 2.71)
+    # С числами с плавающей точкой - тип автоматически определяется как float
+    float_pair = UniversalPair(3.14, 2.71)
     print(f"Пара дробных чисел: {float_pair.get_pair()}")
 
 
--- tasks/universal_pair_package/universal_pair.py
+++ tasks/universal_pair_package/universal_pair.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from typing import Generic, Tuple, TypeVar
 
 T = TypeVar("T")
@@ -9,6 +8,11 @@ T = TypeVar("T")
 
 class UniversalPair(Generic[T]):
     def __init__(self, first: T, second: T) -> None:
+        if type(first) is not type(second):
+            raise TypeError(
+                f"Типы first и second должны совпадать: {type(first)} != {type(second)}"
+            )
+
         self.first = first
         self.second = second
 
